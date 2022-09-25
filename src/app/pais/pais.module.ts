7bfe26f349882bc1abbfc42dfeb51445df93c583
juang-10 <judagosa8520@gmail.com>
2022-09-25 15:41:07 -0500

    feat: agregar y declarar el componente porCapital

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PorCapitalComponent
+  ],
   imports: [
     CommonModule
   ]
