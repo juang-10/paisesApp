35f49bdfc6496a879cb9440825dfb1400c22fa85
juang-10 <judagosa8520@gmail.com>
2022-09-25 15:42:15 -0500

    feat: agregar y declarar el componente porRegion

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pages/por-region/por-region.component';
 
 
 
 @NgModule({
   declarations: [
     PorCapitalComponent,
-    PorPaisComponent
+    PorPaisComponent,
+    PorRegionComponent
   ],
   imports: [
     CommonModule
