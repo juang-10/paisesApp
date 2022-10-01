ffcd0264d7064f716f5cfc5e226aa64cc4a0d3f9
juang-10 <judagosa8520@gmail.com>
2022-10-01 14:32:24 -0500

    feat: estilos para el li

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  styles:[`
+    li {
+      cursor: pointer;
+    }
+  `]
 })
 export class SidebarComponent implements OnInit {
 
