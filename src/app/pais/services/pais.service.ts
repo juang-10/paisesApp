fa8324afb6f1032858ba18e2e627dbd1a0b40968
juang-10 <judagosa8520@gmail.com>
2022-10-02 18:16:51 -0500

    feat: crear servicio de pais

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,20 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { Country } from '../interfaces/pais-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisService {
 
-  private apiUrl: string = 'https://restcountries.com/v3.1/';
+  private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) { }
 
 
-  buscarPais(termino: string): Observable<any> {
+  buscarPais(termino: string): Observable<Country[]> {
     const url = `${ this.apiUrl }/name/${termino}`
-    return this.http.get(url);
+    return this.http.get<Country[]>(url);
   }
 
 }
