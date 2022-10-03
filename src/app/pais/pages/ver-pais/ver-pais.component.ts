d20b133770c3936a1ea27fa3e99633ec4f312c3c
juang-10 <judagosa8520@gmail.com>
2022-10-03 17:59:27 -0500

    ver país de forma independiente

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.paisService.getPaisPorAlpha(id))
+      )
+      .subscribe( resp => {
+        console.log(resp)
+      })
+      // this.activatedRoute.params
+      //   .subscribe( ({id}) => {
+      //     this.paisService.getPaisPorAlpha(id)
+      //       .subscribe({
+      //         next: (pais) => {
+      //           console.log(pais)
+      //         }
+      //       })
+      //   })
+    }
 
 }
