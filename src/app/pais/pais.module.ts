27af388460d8ef117649b15b59b17566f68fcfe4
juang-10 <judagosa8520@gmail.com>
2022-10-02 19:35:54 -0500

    feat: declarar el componente table e input

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -7,6 +7,8 @@ import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { PaisTableComponent } from './components/pais-table/pais-table.component';
+import { PaisInputComponent } from './components/pais-input/pais-input.component';
 
 
 
@@ -15,7 +17,9 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     PorCapitalComponent,
     PorPaisComponent,
     PorRegionComponent,
-    VerPaisComponent
+    VerPaisComponent,
+    PaisTableComponent,
+    PaisInputComponent
   ],
   exports: [
     PorCapitalComponent,
