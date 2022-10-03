6bed5180904c01a809c2cbb3cfe0f25199427409
juang-10 <judagosa8520@gmail.com>
2022-10-03 16:26:20 -0500

    feat: maquetació y logica para el componente por capital

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -6,11 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
 
-  constructor() { }
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
   }
 
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+    this.paisService.buscarCapital(termino)
+      .subscribe((paises) => {
+        this.paises = paises
+      }, (err) => {
+        this.hayError = true;
+        this.paises = [];
+      });
+  }
 }
